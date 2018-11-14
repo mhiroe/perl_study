23b7d2e9fd4a17acc773fb06894514744448fa42
mitsuki-hiroe <mitsuki.hiroe@dena.com>
2018-11-14 09:17:22 +0900

    add

--- orily2/6.6.pl
+++ orily2/6.6.pl
@@ -1,2 +1,16 @@
 use strict;
 use warnings;
+
+my %provisions = (
+'The Skipper' => [qw(blue_shirt hat jacket preserver sunscreen) ],
+'The Professor' => [qw(sunscreen water_bottle slide_rule batteries radio) ],
+'Gilligan' => [qw(red_shirt hat lucky_socks water_bottle) ],
+);
+
+# 持ち物が5こ未満の人
+# print my @packed_light = grep @{ $provisions{$_} } < 5, keys %provisions;
+
+for ( keys %provisions ) {
+  # print if @{$provisions{$_}} <  5;
+  print @{$provisions{$_}};
+}
