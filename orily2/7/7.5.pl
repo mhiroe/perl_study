2a040ddb8d9d5190ca34140a392fabfee86f87f9
mitsuki-hiroe <mitsuki.hiroe@dena.com>
2018-11-20 10:24:30 +0900

    fix

--- orily2/7/7.5.pl
+++ orily2/7/7.5.pl
@@ -6,27 +6,48 @@ use warnings;
 #     my $count = 0;
 #     return sub { print ++$count, ": $File::Find::name\n" };
 # }
-# my $callback = create_find_callback_that_counts(); print "my bin:\n";
+# my $callback = create_find_callback_that_counts();
+# print "my bin:\n";
 # find($callback, 'bin');
 # print "my lib:\n";
 # find($callback, 'lib');
 
+# # 引数を渡して処理を分ける(よくない)
+# use File::Find;
+# sub create_find_callback_that_sums_the_size {
+#   my $total_size = 0;
+#   return sub {
+#     if (@_) { # ダミーの呼び出しの場合
+#       return $total_size;
+#     }
+#     else { # File::Find:からのコールバックの場合
+#       print "$File::Find::name\n";
+#       $total_size += -s if -f;
+#     }
+#   };
+# }
+#
+# my $callback = create_find_callback_that_sums_the_size( );
+# find($callback, 'bin'); # 最初に elseに入って total_size足し上げる
+# find($callback, 'lib'); # 最初に elseに入って total_size足し上げる
+# my $total_size = $callback->('dummy'); # dummy parameter to get size
+# # - @_に入って合計サイズ pringする
+# print "total size of bin is $total_size\n";
+
+# サブルーチンを２つ用意する
 use File::Find;
-sub create_find_callback_that_sums_the_size {
+sub create_find_callbacks_that_sum_the_size {
   my $total_size = 0;
-  return sub {
-    $total_size += -s;
-    # if (@_) { # ダミーの呼び出しの場合
-    #   return $total_size;
-    # }
-    # else { # File::Find:からのコールバックの場合
-    #   $total_size += -s if -f;
-    # }
-  };
+  return(
+    sub { $total_size += -s if -f},
+    sub { return $total_size }
+  );
 }
 
-my $callback = create_find_callback_that_sums_the_size( );
-find($callback, 'bin');
-# my $total_size = $callback->('dummy'); # dummy parameter to get size
-my $total_size = $callback->();
+# ２つのサブルーチンref
+my ($count_em, $get_results) = create_find_callbacks_that_sum_the_size( );
+find($count_em, 'bin'); # コールバックとして呼ぶ
+# my $total_size = &$get_results( );
+# my $total_size = $get_results->(); # find経由しないで 直接呼ぶ
+my $total_size = $count_em->(); # count_em は役割が固定されてしまっている
 print "total size of bin is $total_size\n";
---
+++ orily2/7/7.8.pl
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+
+# 引数がレキシカル変数になり、クロージャ変数になる
+# use File::Find;
+# sub print_bigger_than {
+#   my $minimum_size = shift;
+#   return sub { print "$File::Find::name\n" if -f and -s >= $minimum_size };
+# }
+# my $bigger_than_1024 = \&print_bigger_than(10);
+# print Dumper $bigger_than_1024;
+# # find($bigger_than_1024, 'bin');
+
+
+# サイズが指定したバイト数以上のファイルを見つける
+use File::Spec::Functions qw(canonpath no_upwards);
+use File::Find;
+
+sub find_by_min_size {
+  my $min = shift;
+  my @files = ();
+  sub { push @files, canonpath( $File::Find::name ) if -s $_ >= $min },
+  sub { @files = no_upwards( @files );wantarray ? @files : [ @files ] }
+}
+
+my $find_by_min_size10 = find_by_min_size(10);
+find($find_by_min_size10, 'bin');
+
+# クロージャ変数取り出し方わからん..
+print "@files";
+print
