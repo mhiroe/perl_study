168da0fe20c3ad3ce09de937f644525d69a8cc8f
mitsuki-hiroe <mitsuki.hiroe@dena.com>
2018-11-28 09:21:38 +0900

    fix

--- orily2/10/10.8.pl
+++ orily2/10/10.8.pl
@@ -1,15 +1,18 @@
 use strict;
 use warnings;
 
+use 5.10.0;
+
+# ディレクトリ構造を再帰的に定義する
 sub data_for_path {
   my $path = shift;
   if (-f $path) {
     return undef;
   }
-  if (-d $path) {
+  if (-d $path) { # $pathがディレクトリならば
     my %directory;
     opendir PATH, $path or die "Cannot opendir $path: $!";
-    my @names = readdir PATH; # readdir いくつかディレクトリ名を取得する
+    my @names = readdir PATH; # readdir ディレクトリ内のファイルやディレクトリの名前を取得する
     closedir PATH;
     for my $name (@names) {
       next if $name eq '.' or $name eq '..';
@@ -21,25 +24,29 @@ sub data_for_path {
   return undef;
 }
 
-
 use Data::Dumper;
-print Dumper(data_for_path('../../'));
+# print Dumper(data_for_path('../'));
 
+# 再帰的に定義されたデータを表示する
+# dump_data_for_path('../', data_for_path('../'));
 
-# sub data_for_path {
+my $dir = data_for_path('../7');
+
+say  for (keys $dir->{7});
+# print Dumper $dir;
+
+# sub dump_data_for_path {
 #   my $path = shift;
-#   if (-f $path or -l $path) { # ファイルまたはシンボリックリンクの場合
-#   return undef; }
-#   if (-d $path) {
-#     my %directory;
-#     opendir PATH, $path or die "Cannot opendir $path: $!"; my @names = readdir PATH;
-#     closedir PATH;
-#     for my $name (@names) {
-#       next if $name eq '.' or $name eq '..';
-#       $directory{$name} = data_for_path("$path/$name");
-#     }
-#     return \%directory;
+#   my $data = shift;
+#
+#   if (not defined $data) { # ファイルの場合
+#     print "$path\n";
+#     return;
 #   }
-#   warn "$path is neither a file nor a directory\n";
-#   return undef;
+#
+#   foreach (sort keys %$data) {
+#     # my %directory;
+#     # print $directory{$_};
+#     dump_data_for_path("$path/$_", "$path/$_");
+#   }
 # }
